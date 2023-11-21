4a4c76b541a19d23ad7983d26bc4d213f829f36d
catalunha <catalunha.mj@gmail.com>
2023-11-21 10:01:06 -0300

    Tio Paulo esse é meu primeiro programa no dart

--- bin/list1pag1prog1.dart
+++ bin/list1pag1prog1.dart
@@ -1,5 +1,21 @@
-import 'package:list1pag1prog1/list1pag1prog1.dart' as list1pag1prog1;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${list1pag1prog1.calculate()}!');
+void main() {
+  //Entrada
+  String? digitado;
+  print('Digite o primeiro numero');
+  digitado = stdin.readLineSync();
+  int a = int.parse(digitado ?? '0');
+
+  print('Digite o segundo numero');
+  digitado = stdin.readLineSync();
+  int b = int.parse(digitado ?? '0');
+
+  //Logica
+  int major = a;
+  if (a < b) {
+    major = b;
+  }
+  //Resultado
+  print('between $a and $b the largest is $major');
 }
